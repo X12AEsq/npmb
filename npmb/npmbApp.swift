1754d75c4958adb59f91e54c8fe8b24bd5bd6040
Morris Albers <dagger5@alberslegal.com>
2023-02-25 11:32:49 -0600

    Initial Commit

---
+++ npmb/npmbApp.swift
@@ -0,0 +1,17 @@
+//
+//  npmbApp.swift
+//  npmb
+//
+//  Created by Morris Albers on 2/25/23.
+//
+
+import SwiftUI
+
+@main
+struct npmbApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
